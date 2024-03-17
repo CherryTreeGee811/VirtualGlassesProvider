90a8f5dcd49842011516ef826428455ef4d5b76c
CherryTreeGee811 <79172404+CherryTreeGee811@users.noreply.github.com>
2024-03-17 11:46:41 -0400

    Add extra test for Family Feature - JT 2024-03-17 11:46

--- VirtualGlassesProvider.Tests/FamilyTest.cs
+++ VirtualGlassesProvider.Tests/FamilyTest.cs
@@ -11,13 +11,17 @@ namespace VirtualGlassesProvider.Tests
     {
         #pragma warning disable NUnit1032
         private ChromeDriver _driver { get; set; }
-        #pragma warning restore NUnit1032
+#pragma warning restore NUnit1032
+        private string _downloadPath = Path.GetTempPath();
 
 
         [SetUp]
         public void SetUp()
         {
             ChromeOptions options = new ChromeOptions { AcceptInsecureCertificates = true };
+            options.AddUserProfilePreference("download.default_directory", _downloadPath);
+            options.AddUserProfilePreference("download.prompt_for_download", false);
+            options.AddUserProfilePreference("disable-popup-blocking", "true");
             options.AddArgument("--headless=new");
             _driver = new ChromeDriver(options);
         }
@@ -180,6 +184,48 @@ namespace VirtualGlassesProvider.Tests
 
 
         [Test, Order(4)]
+        public void ClientGeneratesARImageForFamilyMember()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:7044/");
+            _driver.Manage().Window.Size = new System.Drawing.Size(1012, 691);
+            _driver.FindElement(By.Id("login")).Click();
+            _driver.FindElement(By.Id("Input_Email")).Click();
+            _driver.FindElement(By.Id("Input_Email")).SendKeys(TestClient.Email);
+            _driver.FindElement(By.Id("Input_Password")).Click();
+            _driver.FindElement(By.Id("Input_Password")).SendKeys(TestClient.Password);
+            _driver.FindElement(By.Id("login-submit")).Click();
+            var product = _driver.FindElement(By.CssSelector(".col-md-4:nth-child(1) .btn-primary"));
+            new Actions(_driver)
+            .ScrollToElement(product)
+            .Perform();
+            WebDriverWait wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 0, 10));
+            var elem = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(product));
+            elem.Click();
+            _driver.FindElement(By.Id("buyFor")).Click();
+            {
+                var dropdown = _driver.FindElement(By.Id("buyFor"));
+                dropdown.FindElement(By.XPath("//option[. = 'Janet Sinclair']")).Click();
+            }
+            _driver.FindElement(By.CssSelector("#buyFor > option:nth-child(2)")).Click();
+            var preRenderAltText = _driver.FindElement(By.ClassName("detailsImage")).GetAttribute("alt").ToString();
+            Assert.That(preRenderAltText.Equals("Render"), Is.EqualTo(false));
+            _driver.FindElement(By.Id("generateImageBtn")).Click();
+            Thread.Sleep(5000);
+            var renderAltText = _driver.FindElement(By.ClassName("detailsImage")).GetAttribute("alt").ToString();
+            Assert.That(renderAltText, Is.EqualTo("Render"));
+            _driver.FindElement(By.Id("downloadImageLink")).Click();
+            Thread.Sleep(5000);
+            var fileName = $"ARGeneratedImage.jpg";
+            var file = Directory.GetFiles(_downloadPath, fileName, SearchOption.TopDirectoryOnly);
+            Assert.That(file != null);
+            File.Delete(file[0]);
+            _driver.FindElement(By.Id("clearImage")).Click();
+            var revertedAltText = _driver.FindElement(By.ClassName("detailsImage")).GetAttribute("alt").ToString();
+            Assert.That(revertedAltText, Is.EqualTo(preRenderAltText));
+        }
+
+
+        [Test, Order(5)]
         public void ClientDeletesFamilyMember()
         {
             _driver.Navigate().GoToUrl("https://localhost:7044/");
