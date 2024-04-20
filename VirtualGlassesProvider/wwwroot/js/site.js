b0b12ca39e64efe378eb8c841a607de044ab3d55
CherryTreeGee811 <79172404+CherryTreeGee811@users.noreply.github.com>
2024-04-20 15:25:48 -0400

    Add Basic CSP + Other Security Enhancements - JT 2024-04-20

--- VirtualGlassesProvider/wwwroot/js/site.js
+++ VirtualGlassesProvider/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+setTimeout(function () {
+    var addedToCartMessageElement = document.getElementById('addedToCartMessage');
+    if (addedToCartMessageElement) {
+        addedToCartMessageElement.remove();
+    }
+}, 2000);
+
+setTimeout(function () {
+    var addedToWishlistMessageElement = document.getElementById('addedToWishlistMessage');
+    if (addedToWishlistMessageElement) {
+        addedToWishlistMessageElement.remove();
+    }
+}, 2000);
