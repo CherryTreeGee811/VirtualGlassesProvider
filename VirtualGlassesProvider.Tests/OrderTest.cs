6a3453453eb042c7fee828209303360e938ae7c0
CherryTreeGee811 <79172404+CherryTreeGee811@users.noreply.github.com>
2024-04-21 01:03:37 -0400

    Modify Failing Tests - JT 2024-04-21 1:03

--- VirtualGlassesProvider.Tests/OrderTest.cs
+++ VirtualGlassesProvider.Tests/OrderTest.cs
@@ -1,5 +1,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 
 namespace VirtualGlassesProvider.Tests
 {
@@ -37,9 +39,21 @@ namespace VirtualGlassesProvider.Tests
             _driver.FindElement(By.Id("Input_Password")).SendKeys(TestClient.Password);
             _driver.FindElement(By.Id("login-submit")).Click();
             Thread.Sleep(1000);
-            _driver.FindElement(By.CssSelector("body > div > main > div:nth-child(5) > div:nth-child(1) > div > div.card-body > a.btn.btn-success")).Click();
-            _driver.FindElement(By.CssSelector(".fa")).Click();
+            var product = _driver.FindElement(By.CssSelector(".col-md-4:nth-child(1) .btn:nth-child(7)"));
+            new Actions(_driver)
+            .ScrollToElement(product)
+            .Perform();
+            WebDriverWait wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 0, 10));
+            var productElem = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(product));
+            productElem.Click();
+            _driver.FindElement(By.ClassName("fa")).Click();
             _driver.FindElement(By.ClassName("btn-primary")).Click();
+            var checkout = _driver.FindElement(By.ClassName("btn-primary"));
+            new Actions(_driver)
+            .ScrollToElement(checkout)
+            .Perform();
+            var checkoutElem = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(checkout));
+            checkoutElem.Click();
             Assert.That(_driver.FindElement(By.CssSelector(".card-title")).Text, Is.EqualTo("ORDER CONFIRMATION"));
         }
     }
--- VirtualGlassesProvider.Tests/PreconfigurePaymentTest.cs
+++ VirtualGlassesProvider.Tests/PreconfigurePaymentTest.cs
@@ -1,5 +1,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 
 
 namespace VirtualGlassesProvider.Tests
@@ -73,8 +75,14 @@ namespace VirtualGlassesProvider.Tests
             _driver.FindElement(By.ClassName("btn-primary")).Click();
             Assert.That(_driver.FindElement(By.ClassName("alert")).Text, Is.EqualTo("Your Payment Info has been updated"));
             _driver.FindElement(By.CssSelector("body > header > nav > div > div > ul > li:nth-child(1) > a")).Click();
-            _driver.FindElement(By.CssSelector("body > div > main > div:nth-child(5) > div:nth-child(1) > div > div.card-body > a.btn.btn-success")).Click();
-            _driver.FindElement(By.CssSelector(".fa")).Click();
+            var product = _driver.FindElement(By.CssSelector(".col-md-4:nth-child(1) .btn:nth-child(7)"));
+            new Actions(_driver)
+            .ScrollToElement(product)
+            .Perform();
+            WebDriverWait wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 0, 10));
+            var productElem = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(product));
+            productElem.Click();
+            _driver.FindElement(By.ClassName(".fa")).Click();
             var preconfiguredCardHolderName = _driver.FindElement(By.Id("CardHolderName")).GetAttribute("value").ToString();
             var preconfiguredCardNumber = _driver.FindElement(By.Id("CardNumber")).GetAttribute("value").ToString();
             var preconfiguredCvv = _driver.FindElement(By.Id("CVV")).GetAttribute("value").ToString();
