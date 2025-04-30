45d5463e910eef913133129147385b1a6f0c132c
CherryTreeGee811 <79172404+CherryTreeGee811@users.noreply.github.com>
2025-04-30 01:09:29 -0400

    Reapply

--- VirtualGlassesProvider/Migrations/GlassesStoreDbContextModelSnapshot.cs
+++ VirtualGlassesProvider/Migrations/GlassesStoreDbContextModelSnapshot.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VirtualGlassesProvider.Models.DataAccess;
 
+
 namespace VirtualGlassesProvider.Migrations
 {
     [DbContext(typeof(GlassesStoreDbContext))]
